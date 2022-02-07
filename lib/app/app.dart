7bb290fe6463fc75028ce1f12569427ac1e5b7b8
Akanksha Vyas <vyasakanksha@gmail.com>
2022-02-07 14:41:31 +0530

    Router and locator added

---
+++ lib/app/app.dart
@@ -0,0 +1,21 @@
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:my_app/screens/home/services/authentication_service.dart';
+import 'package:my_app/screens/home/services/local_storage_service.dart';
+import 'package:my_app/screens/home/screen.dart';
+
+@StackedApp(
+  logger: StackedLogger(),
+  routes: [
+    MaterialRoute(page: HomeScreen, initial: true),
+  ],
+  dependencies: [
+    LazySingleton(classType: NavigationService),
+    LazySingleton(classType: DialogService),
+    LazySingleton(classType: BottomSheetService),
+    LazySingleton(classType: SnackbarService),
+    LazySingleton(classType: LocalStorageService),
+    LazySingleton(classType: AuthenticationService),
+  ],
+)
+class AppSetup {}
---
+++ lib/app/app.router.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// StackedRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: public_member_api_docs
+
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:stacked/stacked_annotations.dart';
+
+import '../screens/home/screen.dart';
+
+class Routes {
+  static const String homeScreen = '/';
+  static const all = <String>{
+    homeScreen,
+  };
+}
+
+class StackedRouter extends RouterBase {
+  @override
+  List<RouteDef> get routes => _routes;
+  final _routes = <RouteDef>[
+    RouteDef(Routes.homeScreen, page: HomeScreen),
+  ];
+  @override
+  Map<Type, StackedRouteFactory> get pagesMap => _pagesMap;
+  final _pagesMap = <Type, StackedRouteFactory>{
+    HomeScreen: (data) {
+      var args = data.getArgs<HomeScreenArguments>(
+        orElse: () => HomeScreenArguments(),
+      );
+      return MaterialPageRoute<dynamic>(
+        builder: (context) => HomeScreen(key: args.key),
+        settings: data,
+      );
+    },
+  };
+}
+
+/// ************************************************************************
+/// Arguments holder classes
+/// *************************************************************************
+
+/// HomeScreen arguments holder class
+class HomeScreenArguments {
+  final Key? key;
+  HomeScreenArguments({this.key});
+}
