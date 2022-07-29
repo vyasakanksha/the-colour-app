8b5e76b810c8ebdc44e843558fdfc9b4c1f994e3
Akanksha Vyas <vyasakanksha@gmail.com>
2022-07-29 12:05:35 +0530

    var name changes + firebase housekeeping

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/app/app.locator.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:logger/logger.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 // Consts
-import 'package:my_app/consts/colours.dart' as Colours;
-import 'package:my_app/consts/strings.dart' as Strings;
+import 'package:my_app/consts/colours.dart' as _colours;
+import 'package:my_app/consts/strings.dart' as _strings;
 
 // Screens
 import 'package:my_app/screens/home/screen.dart';
 
+// Firebase
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   setupLocator();
 
   runApp(ColourApp());
@@ -22,15 +34,15 @@ class ColourApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: buildTheme(),
-      title: Strings.appName,
-      home: HomeScreen(),
+      title: _strings.appName,
+      home: const HomeScreen(),
       navigatorKey: StackedService.navigatorKey,
     );
   }
 
   ThemeData buildTheme() {
     return ThemeData(
-      primaryColor: Colours.primary,
+      primaryColor: _colours.primary,
     );
   }
 }
