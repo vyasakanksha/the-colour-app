5750ad7adde701305e23ae28591962c444965fc2
Akanksha Vyas <vyasakanksha@gmail.com>
2022-08-11 15:50:30 +0530

    code to read the data in flutter

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -17,45 +17,76 @@ class DatabaseService {
 
   Future addDroplet() async {
     _logger.d("Add Droplet Func");
-    Droplet d = Droplet(timestamp: DateTime.now());
-    DocumentReference docRef = await dropletCollection.add(d.toMap());
-    return docRef.id;
+    // Droplet d = Droplet(timestamp: DateTime.now());
+    // DocumentReference docRef = await dropletCollection.add(d.toMap());
+    // return docRef.id;
   }
 
   Future addColourData(Color colour, String uid) async {
     _logger.d("add colour, $uid $colour");
-    Colour col = Colour(colour: colour, timestamp: DateTime.now());
-    return await dropletCollection
-        .doc(uid)
-        .collection('colours')
-        .add(col.toMap());
+    // Colour col = Colour(colour: colour, timestamp: DateTime.now());
+    // return await dropletCollection
+    //     .doc(uid)
+    //     .collection('colours')
+    //     .add(col.toMap());
   }
 
   Future addLocationData(
       bool permission, Map<String, dynamic> details, String? uid) async {
     _logger.d("add locaton", uid);
-    Location loc = Location(permission: permission, details: details);
-    return await dropletCollection
-        .doc(uid)
-        .collection('location')
-        .add(loc.toMap());
+    // Location loc = Location(permission: permission, details: details);
+    // return await dropletCollection
+    //     .doc(uid)
+    //     .collection('location')
+    //     .add(loc.toMap());
   }
 
   Future addTimeZoneData(String timezone, String? uid) async {
     _logger.d("add timezone", uid);
-    TimeZone tz = TimeZone(timezone: timezone);
-    return await dropletCollection
-        .doc(uid)
-        .collection('timezone')
-        .add(tz.toMap());
+    // TimeZone tz = TimeZone(timezone: timezone);
+    // return await dropletCollection
+    //     .doc(uid)
+    //     .collection('timezone')
+    //     .add(tz.toMap());
   }
 
   Future addDeviceDetailsData(Map<String, dynamic> details, String? uid) async {
     _logger.d("device details", uid);
-    DeviceDetails dd = DeviceDetails(details: details);
-    return await dropletCollection
-        .doc(uid)
-        .collection('device')
-        .add(dd.toMap());
+    //   DeviceDetails dd = DeviceDetails(details: details);
+    //   return await dropletCollection
+    //       .doc(uid)
+    //       .collection('device')
+    //       .add(dd.toMap());
+  }
+
+  Future readData() async {
+    _logger.d("Read Data");
+    final stuff = [];
+
+    QuerySnapshot event = await dropletCollection.get();
+    final data = {};
+    for (var doc in event.docs) {
+      List<String> colours;
+
+      var colSnapshot = await doc.reference
+          .collection("colours")
+          .orderBy("timestamp", descending: true)
+          .limit(1)
+          .get();
+
+      colours = colSnapshot.docs
+          .map((DocumentSnapshot e) => e.data()!.toString())
+          .toList();
+
+      Map<String, dynamic> data = {
+        "id": doc.id,
+        "timestamp": doc.data(),
+        "colours": colours,
+      };
+
+      stuff.add(data);
+    }
+
+    return stuff;
   }
 }
